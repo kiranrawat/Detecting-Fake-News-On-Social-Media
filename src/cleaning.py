5162d9c6c1c61b386658448a28af44a1e0e3c51e
kiranrawat <rawat.kiran111@gmail.com>
2025-11-12 21:13:54 -0800

    modification to clean code and added pos aware lemmatization

--- src/cleaning.py
+++ src/cleaning.py
@@ -4,17 +4,23 @@ Created on Sat Nov 28 11:56:49 2020
 @author: Kiran Rawat
 """
 
-#Import necessary dependencies and settings
+# Import necessary dependencies and settings
 import seaborn as sn
 import re
 import string
 import nltk
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer 
+from nltk.stem import WordNetLemmatizer
 nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+STOPWORDS = set(stopwords.words("english"))
 lemmatizer = WordNetLemmatizer()
 
-#distribution of classes for prediction
+# distribution of classes for prediction
+
+
 def create_distribution(dataFile):
     """ 
     check the data distribution
@@ -25,35 +31,50 @@ def create_distribution(dataFile):
     """
     return sn.countplot(x='label', data=dataFile, palette='hls')
 
+
+def get_pos_tag(word):
+    tag = nltk.pos_tag([word])[0][1][0].upper()
+    pos_dict = {
+        'J': 'a',
+        'N': 'n',
+        'V': 'v',
+        'R': 'r'
+    }
+    return pos_dict.get(tag, 'n')
+
+
 # Cleaning text
+
 def process_text(text):
     """
-    Cleaning the text
-    Args: 
-        text (string): news statement
-    Returns:
-        list of clean text words
+    Clean a text string and return a list of tokens.
+    Optimized and more linguistically correct version.
     """
-    #1  remove special characters\whitespaces\digits
-    text = re.sub(r'[^a-zA-Z\s]', '', text, re.I|re.A)
-    
-    #2 Remove punctuation
-    nopunc = [char for char in text if char not in string.punctuation]
-    nopunc = ''.join(nopunc)
-    
-    #3 Lower case & Remove stopwords
-    clean_words = [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
-    
-    #4 get lemmatized tokens
-    lemmatized_words = [lemmatizer.lemmatize(word) for word in clean_words if (len(word) > 2 and len(word) < 14)]
+    # 1. lowercase
+    text = text.lower()
 
-    return lemmatized_words
+    # 2. keep only letters and whitespace
+    text = re.sub(r"[^a-z\s]", " ", text)
+
+    # 3. tokenize (faster than split and safer)
+    tokens = text.split()
+
+    # 4. remove stopwords
+    tokens = [t for t in tokens if t not in STOPWORDS]
+
+    # 5. # POS-aware lemmatization
+    tokens = [lemmatizer.lemmatize(t, get_pos_tag(t)) for t in tokens]
+
+    return tokens
+
 
 def process_text2(text):
     text1 = " ".join(text)
     return text1
 
 # Mapping the lables into 'True' and 'False' with dictionary
+
+
 def map_lables(train, test, val):
     """
     Args: 
@@ -71,12 +92,12 @@ def map_lables(train, test, val):
     5. False -- False
     6. Pants-fire -- False
     """
-    labels_dict = {'true': 'true','mostly-true': 'true',
-               'half-true':'true', 'false':'false', 
-               'barely-true':'false','pants-fire':'false'}
+    labels_dict = {'true': 'true', 'mostly-true': 'true',
+                   'half-true': 'true', 'false': 'false',
+                   'barely-true': 'false', 'pants-fire': 'false'}
 
-    train= train.replace({"label": labels_dict})[['label','statement']]
-    test = test.replace({"label": labels_dict})[['label','statement']]
-    val = val.replace({"label": labels_dict})[['label','statement']]
-    
-    return train, test, val  
+    train = train.replace({"label": labels_dict})[['label', 'statement']]
+    test = test.replace({"label": labels_dict})[['label', 'statement']]
+    val = val.replace({"label": labels_dict})[['label', 'statement']]
+
+    return train, test, val
